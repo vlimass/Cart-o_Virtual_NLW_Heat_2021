f54488e79be53a6a8ae1b721e8743e8ff32a62ad
Vinícius Lima <villylss@gmail.com>
2021-11-05 15:23:00 -0300

    Conclusão do Stage 4 do projeto

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ function changeSocialMediaLinks(){
 }
 
 changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+    fetch(url)
+    .then(response => response.json()) // Promise 
+    .then(data => {
+        userName.textContent = data.name
+        userLogin.textContent = data.login
+        userLink.href = data.html_url
+        userAvatar.src = data.avatar_url
+        userBio.textContent = data.bio
+    })
+}
+
+getGitHubProfileInfos()
